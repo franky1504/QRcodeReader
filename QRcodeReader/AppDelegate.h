5217d317366822a2f7c6822b00fb6483e9e67464
Franky <franky1504@gmail.com>
2016-02-26 22:55:34 +0800

    Initial Commit

---
+++ QRcodeReader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QRcodeReader
+//
+//  Created by i_feyuwu on 2016/2/26.
+//  Copyright © 2016年 FrankyWu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QRcodeReader/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QRcodeReader
+//
+//  Created by i_feyuwu on 2016/2/26.
+//  Copyright © 2016年 FrankyWu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
